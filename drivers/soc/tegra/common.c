f9e826341bf1826fb3d16295f19ca0023f462e98
jeybee <jethrob@hotmail.com>
2024-04-07 17:35:43 +0100

    add t114 regulator core / cpu coupling, update surface 2 dts

--- drivers/soc/tegra/common.c
+++ drivers/soc/tegra/common.c
@@ -143,7 +143,7 @@ int devm_tegra_core_dev_init_opp_table(struct device *dev,
 		hw_version = BIT(tegra_sku_info.soc_process_id);
 		config.supported_hw = &hw_version;
 		config.supported_hw_count = 1;
-	} else if (of_machine_is_compatible("nvidia,tegra30")) {
+	} else if (of_machine_is_compatible("nvidia,tegra30") || of_machine_is_compatible("nvidia,tegra114")) {
 		hw_version = BIT(tegra_sku_info.soc_speedo_id);
 		config.supported_hw = &hw_version;
 		config.supported_hw_count = 1;
